f8375a62cd3efc6bcaa2edbd51bb73597c0b91cf
ArtN0nymous <jr616934@gmail.com>
2023-01-03 15:17:37 -0700

    ya sube los archivos

--- screens/UserDetails.js
+++ screens/UserDetails.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { View,Text,StyleSheet,Image, Button } from 'react-native';
 import * as DocumentPicker from "expo-document-picker";
 import firebase from '../database/firebase';
 export default function({route,navigation}) {
     const storage = firebase.firebase.storage();
     const {item}=route.params;
+    const [file,setFile] = useState('');
     const selectFile = async()=>{
         try{
             const res = await DocumentPicker.getDocumentAsync({
@@ -12,16 +13,28 @@ export default function({route,navigation}) {
             });
             if(res.type=='cancel'){console.log('cancelado');}
             console.log(res.uri,res.type,res.name,res.size);
+            setFile(res);
         }catch(error){
             console.log(error);
         }
+    }
+    const uploadFile=async()=>{
+        var storageRef = storage.ref('Documentos/'+file.name);
+        let archivo = await fetch(file.uri).then(r => r.blob());
+        storageRef.put(archivo).then(async(snapshot) => {
+            await snapshot.ref.getDownloadURL().then((result)=>{
+                console.log('Link:  ',result);
+            });
+        },(error)=>{
+            console.log(error.code,error.message);
+        });
     }
   return (
     <View style={styles.fondo}>
         <Image source={{uri:'https://www.pngall.com/wp-content/uploads/5/User-Profile-PNG-High-Quality-Image.png'}} style={styles.img_user}/>
         <Text style={styles.text_details}>{item.name}</Text>
         <Button title='Cargar archivo' style={styles.btn_files} onPress={()=>selectFile()}/>
-        <Button title='Enviar archivo' style={styles.btn_files}/>
+        <Button title='Enviar archivo' style={styles.btn_files} onPress={()=>uploadFile()}/>
     </View>
   )
 }
